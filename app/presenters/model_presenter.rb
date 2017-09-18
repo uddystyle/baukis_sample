7c54f2850d0deee8f8fed7636335d39722ee98ec
Tomohisa Uchida <uddystyle@gmail.com>
2017-09-18 13:23:03 +0900

    Finished ex16-1

---
+++ app/presenters/model_presenter.rb
@@ -0,0 +1,10 @@
+class ModelPresenter
+	attr_reader :object, :view_context
+	delegate :raw, to: :view_context
+
+	def initialize(object, view_context)
+		@object = object
+		@view_context = view_context
+	end
+
+end
