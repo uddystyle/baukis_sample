5d833c6f89c482c84cbacbd25ea71dfb8507f8db
Tomohisa Uchida <uddystyle@gmail.com>
2017-10-01 14:16:32 +0900

    Finished 16-3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  include HtmlBuilder
 
   def document_title
     if @title.present?
--- app/presenters/staff_member_presenter.rb
+++ app/presenters/staff_member_presenter.rb
@@ -1,10 +1,10 @@
 class StaffMemberPresenter < ModelPresenter
-	delegate :suspended?, to: :object
+  delegate :suspended?, to: :object
 
-	# 職員の停止フラグOn/Offを表現する記号を返す。
-	# On: BALLOT BOX WITH CHECK (U+2611)
-	# Off: BALLOT BOX (U+2610)
-	def suspended_mark
-		suspended? ? raw('&#x2611;') : raw('&#x2610;')
-	end
+  # 職員の停止フラグOn/Offを表現する記号を返す。
+  # On: BALLOT BOX WITH CHECK (U+2611)
+  # Off: BALLOT BOX (U+2610)
+  def suspended_mark
+    suspended? ? raw('&#x2611;') : raw('&#x2610;')
+  end
 end
